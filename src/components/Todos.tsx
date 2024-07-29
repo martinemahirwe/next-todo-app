f033480d2267e1710a1fdcae29d4f7c4eacfffc5
martinemahirwe <martinemahirwe@gmail.com>
2024-07-29 12:49:32 +0200

    ft: implementing the use of tanstack for state management replacing fetching and fetch using tanstack querry

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,39 +1,69 @@
 "use client";
-import { FC, useState } from "react";
+import { FC } from "react";
 import { todoType } from "@/types/todoType";
 import TodoItem from "./TodoItem";
 import TodoForm from "./TodoForm";
 import { createTask, removeTask, updateTaskStatus, updateTask } from "@/actions/todoActions";
 import { v4 as uuidv4 } from 'uuid';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface Props {
-  todos: todoType[];
+  todos: todoType[] | undefined;
 }
 
 const Todos: FC<Props> = ({ todos }) => {
+  const queryClient = useQueryClient();
+
+  const createTaskMutation = useMutation({
+    mutationFn: ({ id, task }: { id: string; task: string }) => createTask(id, task),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    }
+  });
+
+  const updateTaskStatusMutation = useMutation({
+    mutationFn: ({ id, completed }: { id: string; completed: boolean }) => updateTaskStatus(id, completed),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    }
+  });
+
+  const updateTaskMutation = useMutation({
+    mutationFn: ({ id, task }: { id: string; task: string }) => updateTask(id, task),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    }
+  });
+
+  const removeTaskMutation = useMutation({
+    mutationFn: (id: string) => removeTask(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    }
+  });
 
   const createTodo = (task: string) => {
     const id = uuidv4();
-    createTask(id, task);
+    createTaskMutation.mutate({ id, task });
   };
 
   const changeTodoText = (id: string, task: string) => {
-    updateTask(id, task);
+    updateTaskMutation.mutate({ id, task });
   };
 
   const toggleIsTodoDone = (id: string, completed: boolean) => {
-    updateTaskStatus(id, completed);
+    updateTaskStatusMutation.mutate({ id, completed });
   };
 
   const deleteTodoItem = (id: string) => {
-    removeTask(id);
+    removeTaskMutation.mutate(id);
   };
 
   return (
     <main className="flex flex-col text-center">
       <TodoForm AddTodo={createTodo} />
       <div className="flex flex-col mt-8 gap-2">
-        {todos.map((todo) => (
+        {todos?.map((todo) => (
           <TodoItem
             key={todo.id}
             todo={todo}
--- src/types/todoType.ts
+++ src/types/todoType.ts
@@ -1,6 +1,12 @@
+import { ReactNode } from "react";
+
 export type todoType = {
     id: string;
     task: string;
     completed: boolean;
   };
   
+ export interface RootLayoutProps {
+    children: ReactNode;
+    dehydratedState?: unknown; 
+  }
